f96e0886065ba0769026c7cc0a4e7a7ba0826114
Lim sumin <51976580+Superdex517@users.noreply.github.com>
2021-02-23 18:43:57 +0900

    나머지 항목 만들기

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
+import { TodoComponent } from './components/page/item/todo.js';
+import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
-import { PageComponent } from "./components/page/page.js";
+import { PageComponent } from './components/page/page.js';
 
 class App{
     private readonly page: PageComponent;
@@ -9,6 +11,12 @@ class App{
 
         const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
         image.attachTo(appRoot, 'beforeend');
+
+        const note = new NoteComponent('Note Title', 'Note body');
+        note.attachTo(appRoot, 'beforeend');
+
+        const todo = new TodoComponent('Todo Title', 'Todo Item');
+        todo.attachTo(appRoot, 'beforeend');
     }
 }
 
